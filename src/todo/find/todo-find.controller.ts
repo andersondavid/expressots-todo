83df30bde6622be16e91071491143e1cfa0d2c86
Anderson David <andersondavidfreitas@gmail.com>
2023-06-28 23:29:56 -0300

    reafactor: todo find use case

--- src/todo/find/todo-find.controller.ts
+++ src/todo/find/todo-find.controller.ts
@@ -3,7 +3,6 @@ import { controller, httpGet, requestParam, response } from "inversify-express-u
 import { Response } from "express";
 import { ITodoFindRequestDTO, ITodoFindResponseDTO } from './todo-find.dto'
 import { FindUseCase } from './todo-find.usecase'
-import { log } from "console";
 
 @controller("/todo/find")
 class FindController extends BaseController {
@@ -14,13 +13,12 @@ class FindController extends BaseController {
 	@httpGet("/:id")
 	execute(@requestParam() payload: ITodoFindRequestDTO, @response() res: Response): ITodoFindResponseDTO {
 
-		{
-			return this.callUseCase(
-				this.todoFindUseCase.execute(payload),
-				res,
-				StatusCode.OK
-			)
-		}
+		return this.callUseCase(
+			this.todoFindUseCase.execute(payload),
+			res,
+			StatusCode.OK
+		)
+
 	}
 }
 
