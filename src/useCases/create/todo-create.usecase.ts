86bdff0aaba085461a7349f171f1afbf36be2b0b
Anderson David <andersondavidfreitas@gmail.com>
2023-06-29 23:46:15 -0300

    feat: Change repository by factory

--- src/useCases/create/todo-create.usecase.ts
+++ src/useCases/create/todo-create.usecase.ts
@@ -1,15 +1,15 @@
 import { provide } from "inversify-binding-decorators";
 import { ITodoCreateRequestDTO, ITodoCreateResponseDTO } from './todo-create.dto'
-import { ToDoRepository } from "../../repository/todo/todo-repository";
 import { Report, StatusCode } from "@expressots/core";
+import { ToDoFactory } from "../../factories/todo-factories";
 
 
 @provide(TodoCreateUseCase)
 class TodoCreateUseCase {
-	constructor(private todoRepository: ToDoRepository) { }
+	constructor(private todoFactory: ToDoFactory) { }
 	execute(payload: ITodoCreateRequestDTO): ITodoCreateResponseDTO | null {
 		try {
-			const todo = this.todoRepository.create({id: '123', content: payload.content})
+			const todo = this.todoFactory.create({ content: payload.content })
 
 			if (todo?.id === undefined) {
 				Report.Error("ToDo Not Created", StatusCode.BadRequest, "todo-create-usecase");
