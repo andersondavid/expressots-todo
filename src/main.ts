d1cec5309c5cde25330c9d2c2b86308f9318dc40
Anderson David <andersondavidfreitas@gmail.com>
2023-06-29 23:19:59 -0300

    feat: Add version and name

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { container } from "./app.container";
 
 async function bootstrap() {
     AppInstance.create(container);
-    AppInstance.listen(3000, ServerEnvironment.Development);
+    AppInstance.listen(3000, ServerEnvironment.Development, {
+        appName: 'ToDo ExpressoTS',
+        appVersion: '1.0.0'
+    });
 }
 
 bootstrap();
