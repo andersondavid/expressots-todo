e16fecb539e0d489d404b04945ce944dccdfd343
Anderson David <andersondavidfreitas@gmail.com>
2023-07-02 23:06:21 -0300

    feat: Add  update to modules

--- src/useCases/todo.module.ts
+++ src/useCases/todo.module.ts
@@ -2,8 +2,9 @@ import { CreateModule } from "@expressots/core";
 import { FindController } from "./find/todo-find.controller";
 import { TodoCreateController } from "./create/todo-create.controller";
 import { TodoFindallController } from "./findall/todo-findall.controller";
+import { TodoUpdateController } from "./update/todo-update.controller";
 
 
-const TodoModule = CreateModule([FindController, TodoCreateController, TodoFindallController]);
+const TodoModule = CreateModule([FindController, TodoCreateController, TodoFindallController, TodoUpdateController]);
 
 export { TodoModule };
