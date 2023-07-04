cc66e0106057d1c05012d4b01838b88bfe4f1720
Anderson David <andersondavidfreitas@gmail.com>
2023-07-03 23:38:25 -0300

    feat: Add delete to modules

--- src/useCases/todo.module.ts
+++ src/useCases/todo.module.ts
@@ -3,8 +3,9 @@ import { FindController } from "./find/todo-find.controller";
 import { TodoCreateController } from "./create/todo-create.controller";
 import { TodoFindallController } from "./findall/todo-findall.controller";
 import { TodoUpdateController } from "./update/todo-update.controller";
+import { TodoDeleteController } from "./delete/todo-delete.controller";
 
 
-const TodoModule = CreateModule([FindController, TodoCreateController, TodoFindallController, TodoUpdateController]);
+const TodoModule = CreateModule([FindController, TodoCreateController, TodoFindallController, TodoUpdateController, TodoDeleteController]);
 
 export { TodoModule };
